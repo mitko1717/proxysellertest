29f479d0c3cfa54dbab878582cdeb9dffa48a318
mitko1717 <62745604+mitko1717@users.noreply.github.com>
2022-01-14 13:03:28 +0200

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import fetchDataReducer from "../features/users/slice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     fetch: fetchDataReducer,
   },
 });
